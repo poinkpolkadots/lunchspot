93b96edbb3b695a03ae8a89884fcc94405a81710
Qui est-ce que je suis? Qui sait? <hcps-bawoldg@henricostudents.org>
2025-01-13 13:53:20 -0500

    Create Utqiagvik.js
    This is the program that will retrieve information from a spreadsheet and convert it into a JS objects that can be imported from the ClassPagesPrototype.

---
+++ Utqiagvik.js
@@ -0,0 +1,55 @@
+function lastNaams() {
+  //set where last names are stored
+  a=[];
+  return a;
+}
+function firstNaams() {
+  //set wher first names are stored
+  a=[];
+  return a;
+}
+function classNumbs() {
+  //set where class numbers are stored
+  a=[];
+  return a;
+}
+function floorNumbs() {
+  //set where floor numbers are stored
+  a=[];
+  return a;
+}
+function depts() {
+  //set where departement names are stored
+  a=[];
+  return a;
+}
+function quandOuvris() {
+  //List containing information as to when a class is open on the current day
+  a=[];
+  return a;
+}
+function opinCloseds() {
+  //List containing information on whether the room is open or not
+  a=[];
+  return a;
+}
+function statuss() {
+  //List containing class room statuses
+  a=[];
+  return a;
+}
+function noticesSets() {
+  //List contains set of notices for respective classes
+  a=[];
+  return a;
+}
+function dayByDaySchedules() {
+  //List contains weekly schedules for respective classes
+  a=[];
+  return a;
+}
+function possiblyFilleds() {
+  //List contains information on how full a room is
+  a=[];
+  return a;
+}
